c1fa429bdadaf57ff84a7c096b3969fd8e307824
akihiro <aponmaru@gmail.com>
2024-12-26 18:04:07 +0900

    add: サブコマンド作成と --help への説明追加

--- kakeibo/src/main.rs
+++ kakeibo/src/main.rs
@@ -1,12 +1,26 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[clap(version = "1.0")]
-struct Args {
-    arg1: String,
-    arg2: String,
+struct App {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// 新しい講座を作る
+    New,
+    /// 口座に入金する
+    Deposit,
+    /// 口座から出金する
+    Withdraw,
+    /// CSV からインポートする
+    Import,
+    /// レポートを出力する
+    Report,
 }
 
 fn main() {
-    let _args = Args::parse();
+    let _args = App::parse();
 }
