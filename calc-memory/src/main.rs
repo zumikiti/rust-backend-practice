994b2ad4f662fedba1472a4be59e22d0bf9a02c2
akihiro <aponmaru@gmail.com>
2024-12-26 13:48:48 +0900

    modify: 計算式をメソッド化

--- calc-memory/src/main.rs
+++ calc-memory/src/main.rs
@@ -15,10 +15,10 @@ fn main() {
         let left:f64 = tokens[0].parse().unwrap();
         let right:f64 = tokens[2].parse().unwrap();
         let result = match tokens[1] {
-            "+" => left + right,
-            "-" => left - right,
-            "*" => left * right,
-            "/" => left / right,
+            "+" => add_value(left, right),
+            "-" => subtract_value(left, right),
+            "*" => multiply_value(left, right),
+            "/" => divide_value(left, right),
             _ => {
                 // 入力が正しい場合は、ここに来ない
                 unreachable!()
@@ -26,6 +26,26 @@ fn main() {
         };
 
         // 結果表示
-        println!(" => {}", result)
+        print_value(result)
+    }
+
+    fn print_value(value: f64) {
+        println!(" => {}", value)
+    }
+
+    fn add_value(left: f64, right: f64) -> f64 {
+      left + right
+    }
+
+    fn subtract_value(left: f64, right: f64) -> f64 {
+      left - right
+    }
+
+    fn multiply_value(left: f64, right: f64) -> f64 {
+      left * right
+    }
+
+    fn divide_value(left: f64, right: f64) -> f64 {
+      left / right
     }
 }
