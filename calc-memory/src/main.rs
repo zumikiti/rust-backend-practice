d7851c9ec5a0ca75d55ea1f75092c9d59a0be3c9
akihiro <aponmaru@gmail.com>
2024-12-26 15:39:49 +0900

    modify: メモリ構造体を設定

--- calc-memory/src/main.rs
+++ calc-memory/src/main.rs
@@ -1,7 +1,9 @@
 use std::io::stdin;
 
 fn main() {
-    let mut memories: Vec<f64> = vec![0.0; 10];
+    let mut memory = Memory {
+        slots: vec![0.0; 10],
+    };
     let mut prev_result: f64 = 0.0;
 
     for line in stdin().lines() {
@@ -17,16 +19,16 @@ fn main() {
         // メモリへの書き込み
         let is_memory = tokens[0].starts_with("mem");
         if is_memory && tokens[0].ends_with("+") {
-            add_and_print_memory(&mut memories, tokens[0], prev_result);
+            add_and_print_memory(&mut memory, tokens[0], prev_result);
             continue;
         } else if is_memory && tokens[0].ends_with("-") {
-            add_and_print_memory(&mut memories, tokens[0], -prev_result);
+            add_and_print_memory(&mut memory, tokens[0], -prev_result);
             continue;
         }
 
         // 計算
-        let left = eval_token(tokens[0], &memories);
-        let right = eval_token(tokens[2], &memories);
+        let left = eval_token(tokens[0], &memory);
+        let right = eval_token(tokens[2], &memory);
         let result = eval_expression(left, tokens[1], right);
 
         // 結果表示
@@ -40,20 +42,31 @@ fn print_value(value: f64) {
     println!(" => {}", value)
 }
 
+struct Memory {
+    slots: Vec<f64>,
+}
+
 fn add_and_print_memory(
-    memories: &mut [f64],
+    memory: &mut Memory,
     token: &str,
     prev_result: f64,
 ) {
-    let slot_index: usize = token[3..token.len() - 1].parse().unwrap();
-    memories[slot_index] += prev_result;
-    print_value(memories[slot_index])
+    let slot_index: usize = match token[3..token.len() - 1].parse() {
+        Ok(index) => index,
+        Err(_) => {
+            println!("token len {}", token.len());
+            println!("Failed to parse slot index from token: {}", token);
+            return;
+        }
+    };
+    memory.slots[slot_index] += prev_result;
+    print_value(memory.slots[slot_index])
 }
 
-fn eval_token(token: &str, memories: &[f64]) -> f64 {
+fn eval_token(token: &str, memory: &Memory) -> f64 {
     if token.starts_with("mem") {
         let slot_index: usize = token[3..].parse().unwrap();
-        memories[slot_index]
+        memory.slots[slot_index]
     } else {
         token.parse().unwrap()
     }
