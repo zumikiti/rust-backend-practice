ba5e44b1f07471cf8f8c18bd6fa15a6ca7a70521
akihiro <aponmaru@gmail.com>
2024-12-26 15:00:46 +0900

    modify: メモリ 10 個使用できるように変更 WIP

--- calc-memory/src/main.rs
+++ calc-memory/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::stdin;
 
 fn main() {
-    let mut memory: f64 = 0.0;
+    let mut memories: Vec<f64> = vec![0.0; 10];
     let mut prev_result: f64 = 0.0;
 
     for line in stdin().lines() {
@@ -15,21 +15,18 @@ fn main() {
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
 
         // メモリへの書き込み
-        if tokens[0] == "mem+" {
-            memory += prev_result;
-            print_value(memory);
-
+        let is_memory = tokens[0].starts_with("mem");
+        if is_memory && tokens[0].ends_with("+") {
+            add_and_print_memory(&mut memories, tokens[0], prev_result);
             continue;
-        } else if tokens[0] == "mem-" {
-            memory -= prev_result;
-            print_value(memory);
-
+        } else if is_memory && tokens[0].ends_with("-") {
+            add_and_print_memory(&mut memories, tokens[0], -prev_result);
             continue;
         }
 
         // 計算
-        let left = eval_token(tokens[0], memory);
-        let right = eval_token(tokens[2], memory);
+        let left = eval_token(tokens[0], memories);
+        let right = eval_token(tokens[2], memories);
         let result = eval_expression(left, tokens[1], right);
 
         // 結果表示
@@ -37,29 +34,36 @@ fn main() {
 
         prev_result = result
     }
+}
 
-    fn print_value(value: f64) {
-        println!(" => {}", value)
+fn print_value(value: f64) {
+    println!(" => {}", value)
+}
+
+fn add_and_print_memory(memories: &mut Vec<f64>, token: &str, prev_result: f64) {
+    let slot_index: usize = token[3..token.len() - 1].parse().unwrap();
+    memories[slot_index] += prev_result;
+    print_value(memories[slot_index])
+}
+
+fn eval_token(token: &str, memories: Vec<f64>) -> f64 {
+    if token.starts_with("mem") {
+        let slot_index: usize = token[3..].parse().unwrap();
+        memories[slot_index]
+    } else {
+        token.parse().unwrap()
     }
+}
 
-    fn eval_token(token: &str, memory: f64) -> f64 {
-        if token == "mem" {
-            memory
-        } else {
-            token.parse().unwrap()
-        }
-    }
-
-    fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
-        match operator {
-            "+" => left + right,
-            "-" => left - right,
-            "*" => left * right,
-            "/" => left / right,
-            _ => {
-                // 入力が正しい場合は、ここに来ない
-                unreachable!()
-            }
+fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
+    match operator {
+        "+" => left + right,
+        "-" => left - right,
+        "*" => left * right,
+        "/" => left / right,
+        _ => {
+            // 入力が正しい場合は、ここに来ない
+            unreachable!()
         }
     }
 }
