55ec23dd19f7c3bbb9f4cf9ce10f7a69e85c5685
akihiro <aponmaru@gmail.com>
2024-12-27 17:00:51 +0900

    add: 並列処理に実装変更

--- kakeibo/thumbnail-tool/README.md
+++ kakeibo/thumbnail-tool/README.md
@@ -10,3 +10,14 @@ Executed in   31.75 secs    fish           external
    usr time   31.60 secs   57.00 micros   31.60 secs
    sys time    0.14 secs  682.00 micros    0.14 secs
 ```
+
+並列処理の場合
+```sh
+$ time ./target/debug/thumbnail-tool ~/Desktop ./output
+Processed 72 Images
+
+________________________________________________________
+Executed in   10.53 secs    fish           external
+   usr time   33.73 secs   78.00 micros   33.73 secs
+   sys time    0.13 secs  807.00 micros    0.13 secs
+```
--- kakeibo/thumbnail-tool/src/main.rs
+++ kakeibo/thumbnail-tool/src/main.rs
@@ -1,5 +1,10 @@
-use std::{fs::{create_dir_all, read_dir}, path::PathBuf};
 use clap::Parser;
+use std::{
+    fs::{create_dir_all, read_dir},
+    path::PathBuf,
+    sync::{Arc, Mutex},
+    thread,
+};
 
 #[derive(Parser)]
 struct Args {
@@ -15,25 +20,47 @@ fn main() {
     // 出力先フォルダ
     create_dir_all(&args.output).unwrap();
 
-    let mut processed_count = 0;
-    for item in read_dir(&args.input).unwrap() {
-        let item = item.unwrap();
-        let input_path = item.path();
-        if input_path.is_dir() {
+    let mut all_paths = vec![];
+    for entry in read_dir(&args.input).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_dir() {
             // フォルダは処理しない
             continue;
         }
 
-        let img = image::open(&input_path);
-        if let Ok(img) = img {
-            let thumbnail = img.thumbnail(64, 64);
-            let output_path = args.output.join(
-                input_path.file_name().unwrap()
-            );
-            thumbnail.save(output_path).unwrap();
-            processed_count += 1
-        }
+        all_paths.push(path)
     }
 
-    println!("Processed {} Images", processed_count)
+    let processed_count = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for check in all_paths.chunks((all_paths.len() + 3) / 4) {
+        let check = check.to_vec();
+        let processed_count = processed_count.clone();
+        let output = args.output.clone();
+
+        handles.push(thread::spawn(move || {
+            let mut local_count = 0;
+            for path in check {
+                let output_path = output.join(path.file_name().unwrap());
+                let img = image::open(&path);
+                if let Ok(img) = img {
+                    let thumbnail = img.thumbnail(64, 64);
+                    thumbnail.save(output_path).unwrap();
+                    local_count += 1
+                }
+            }
+            let mut writer = processed_count.lock().unwrap();
+            *writer += local_count
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap()
+    }
+
+    println!(
+        "Processed {} Images",
+        processed_count.as_ref().lock().unwrap()
+    )
 }
