cb7817cb7e07b0990747529dc8d5b66c1e7a675d
akihiro <aponmaru@gmail.com>
2024-12-27 17:17:57 +0900

    modify: channel での並列処理実装

--- kakeibo/thumbnail-tool/src/main.rs
+++ kakeibo/thumbnail-tool/src/main.rs
@@ -2,8 +2,8 @@ use clap::Parser;
 use std::{
     fs::{create_dir_all, read_dir},
     path::PathBuf,
-    sync::{Arc, Mutex},
-    thread,
+    sync::mpsc::channel,
+    thread, usize,
 };
 
 #[derive(Parser)]
@@ -20,47 +20,47 @@ fn main() {
     // 出力先フォルダ
     create_dir_all(&args.output).unwrap();
 
-    let mut all_paths = vec![];
-    for entry in read_dir(&args.input).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        if path.is_dir() {
-            // フォルダは処理しない
-            continue;
-        }
-
-        all_paths.push(path)
-    }
-
-    let processed_count = Arc::new(Mutex::new(0));
     let mut handles = vec![];
-    for check in all_paths.chunks((all_paths.len() + 3) / 4) {
-        let check = check.to_vec();
-        let processed_count = processed_count.clone();
+    let mut channels = vec![];
+    let (counter_tx, counter_rx) = channel::<usize>();
+    for _ in 0..4 {
+        let (tx, rx) = channel::<PathBuf>();
+        channels.push(tx);
+        let counter_tx = counter_tx.clone();
         let output = args.output.clone();
-
         handles.push(thread::spawn(move || {
-            let mut local_count = 0;
-            for path in check {
+            while let Ok(path) = rx.recv() {
                 let output_path = output.join(path.file_name().unwrap());
                 let img = image::open(&path);
                 if let Ok(img) = img {
                     let thumbnail = img.thumbnail(64, 64);
                     thumbnail.save(output_path).unwrap();
-                    local_count += 1
+
+                    counter_tx.send(1).unwrap()
                 }
             }
-            let mut writer = processed_count.lock().unwrap();
-            *writer += local_count
         }));
     }
 
+    // 送信側は画像ファイルのパスを送信する
+    for (index, item) in read_dir(&args.input).unwrap().enumerate() {
+        let item = item.unwrap();
+        let path = item.path();
+        if path.is_dir() {
+            continue;
+        }
+
+        channels[index % channels.len()].send(path).unwrap()
+    }
+
+    // 処理の完了通知
+    for channel in channels {
+        drop(channel)
+    }
+    drop(counter_tx);
     for handle in handles {
         handle.join().unwrap()
     }
 
-    println!(
-        "Processed {} Images",
-        processed_count.as_ref().lock().unwrap()
-    )
+    println!("Processed {} Images", counter_rx.iter().count(),)
 }
