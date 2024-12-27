ec863de1f8e699ac4f848281fb3de397bcb49975
akihiro <aponmaru@gmail.com>
2024-12-27 14:56:16 +0900

    add: withdraw サブコマンドで出勤を登録できるよう実装

--- kakeibo/src/main.rs
+++ kakeibo/src/main.rs
@@ -18,7 +18,7 @@ enum Command {
     /// 口座に入金する
     Deposit(DepositArgs),
     /// 口座から出金する
-    Withdraw,
+    Withdraw(WithdrawArgs),
     /// CSV からインポートする
     Import,
     /// レポートを出力する
@@ -66,13 +66,40 @@ impl DepositArgs {
     }
 }
 
+#[derive(Args)]
+struct WithdrawArgs {
+    account_name: String,
+    date: NaiveDate,
+    usage: String,
+    amount: u32,
+}
+
+impl WithdrawArgs {
+    fn run(&self) {
+        let open_option = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("{}.csv", self.account_name))
+            .unwrap();
+        let mut writer = Writer::from_writer(open_option);
+        writer
+            .write_record(&[
+                self.date.format("%Y-%m-%d").to_string(),
+                self.usage.to_string(),
+                format!("-{}", self.amount),
+            ])
+            .unwrap();
+        writer.flush().unwrap()
+    }
+}
+
 fn main() {
     let args = App::parse();
 
     match args.command {
         Command::New(args) => args.run(),
         Command::Deposit(args) => args.run(),
-        Command::Withdraw => unimplemented!(),
+        Command::Withdraw(args) => args.run(),
         Command::Import => unimplemented!(),
         Command::Report => unimplemented!(),
     }
