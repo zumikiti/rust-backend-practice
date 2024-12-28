7f64c89440b3d7d38baab736d4f25e7c47b0971f
akihiro <aponmaru@gmail.com>
2024-12-29 06:48:03 +0900

    add: rayon を使用しての並列処理

--- kakeibo/thumbnail-tool/src/main.rs
+++ kakeibo/thumbnail-tool/src/main.rs
@@ -1,9 +1,8 @@
 use clap::Parser;
+use rayon::prelude::*;
 use std::{
     fs::{create_dir_all, read_dir},
     path::PathBuf,
-    sync::mpsc::channel,
-    thread, usize,
 };
 
 #[derive(Parser)]
@@ -20,47 +19,22 @@ fn main() {
     // 出力先フォルダ
     create_dir_all(&args.output).unwrap();
 
-    let mut handles = vec![];
-    let mut channels = vec![];
-    let (counter_tx, counter_rx) = channel::<usize>();
-    for _ in 0..4 {
-        let (tx, rx) = channel::<PathBuf>();
-        channels.push(tx);
-        let counter_tx = counter_tx.clone();
-        let output = args.output.clone();
-        handles.push(thread::spawn(move || {
-            while let Ok(path) = rx.recv() {
-                let output_path = output.join(path.file_name().unwrap());
-                let img = image::open(&path);
-                if let Ok(img) = img {
-                    let thumbnail = img.thumbnail(64, 64);
-                    thumbnail.save(output_path).unwrap();
-
-                    counter_tx.send(1).unwrap()
-                }
-            }
-        }));
-    }
-
-    // 送信側は画像ファイルのパスを送信する
-    for (index, item) in read_dir(&args.input).unwrap().enumerate() {
+    let items: Vec<_> = read_dir(&args.input).unwrap().collect();
+    // rayon クレートを使い、 into_par_iter() を使用する
+    let result = items.into_par_iter().map(|item| {
         let item = item.unwrap();
         let path = item.path();
-        if path.is_dir() {
-            continue;
+        let output_path = args.output.join(path.file_name().unwrap());
+        let img = image::open(&path);
+        if let Ok(img) = img {
+            let thumbnail = img.thumbnail(64, 64);
+            thumbnail.save(output_path).unwrap();
+
+            1
+        } else {
+            0
         }
+    });
 
-        channels[index % channels.len()].send(path).unwrap()
-    }
-
-    // 処理の完了通知
-    for channel in channels {
-        drop(channel)
-    }
-    drop(counter_tx);
-    for handle in handles {
-        handle.join().unwrap()
-    }
-
-    println!("Processed {} Images", counter_rx.iter().count(),)
+    println!("Processed {} Images", result.sum::<u32>())
 }
